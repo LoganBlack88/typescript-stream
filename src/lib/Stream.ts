362774c12c0e92623cff8fdba501639436b86ece
Logan Black <lblack@glhec.org>
2018-09-12 12:17:52 -0500

    Add some methods and tests.

--- src/lib/Stream.ts
+++ src/lib/Stream.ts
@@ -1,7 +1,8 @@
 
 export class Stream<T> {
 
-    private array: T[];
+    private array: Array<T>;
+
 
     private constructor(array: Array<T>) {
         this.array = array;
@@ -11,5 +12,50 @@ export class Stream<T> {
         return new Stream(array);
     }
 
+    public filter(predicate: (a: T) => boolean): Stream<T> {
+        const results: T[] = [];
+        this.array.forEach((value: T) => {
+            if (predicate(value)) {
+                results.push(value);
+            }
+        });
+        return new Stream(results);
+    }
+
+    public map<R>(mapper: (a: T) => R): Stream<R> {
+        const results: R[] = [];
+        this.array.forEach((item: T, index: number, array: T[]) => {
+            results.push(mapper(item));
+        });
+        return new Stream(results);
+    }
+
+    public max(comparator?: (a1: T, a2: T) => number): T {
+        let max: T = this.array[0];
+        this.array.forEach((value: T, index: number) => {
+            if ((!max || index < 1)
+                || (!comparator && value > max)
+                || (comparator && comparator(value, max) > 0)) {
+                max = value;
+            }
+        });
+        return max;
+    }
+
+    public min(comparator?: (a1: T, a2: T) => number): T {
+        let min: T = this.array[0];
+        this.array.forEach((value: T, index: number) => {
+            if ((!min || index < 1)
+                || (!comparator && value < min)
+                || (comparator && comparator(value, min) < 0)) {
+                min = value;
+            }
+        });
+        return min;
+    }
+
+    public toArray(): Array<T> {
+        return this.array;
+    }
 
 }
---
+++ src/test/stream.spec.ts
@@ -0,0 +1,65 @@
+import { Stream } from '../lib/Stream';
+import { expect, assert } from 'chai';
+
+
+describe('Stream', () => {
+    describe('.of()', () => {
+        it('should instantiate a new Stream', () => {
+            const stream = Stream.of([1,2,3]);
+            assert.instanceOf(stream, Stream);
+        });
+    });
+
+    describe('.filter()', () => {
+        it('should only return values for which the predicate returns truthy.', () => {
+            const evens = Stream.of([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+                                .filter((val: number) => val % 2 === 0)
+                                .toArray();
+
+            expect(evens.length).to.equal(5);
+            expect(evens).to.have.same.members([2,4,6,8,10]);
+        });
+    });
+
+    describe('.map()', () => {
+        it('should convert the item value from one type to another', () => {
+           const nums: number[] = Stream.of(['1', '2', '3', '4', '5'])
+                                        .map((val: string) => parseInt(val))
+                                        .toArray();
+
+           expect(nums.length).to.equal(5);
+           expect(nums).to.have.same.members([1, 2, 3, 4, 5]);
+        });
+    });
+
+    describe('.max()', () => {
+        it('should return the maximum value using the ">" operator if no comparator is provided', () => {
+            const max: number = Stream.of([2,4,5,1,3])
+                                      .max();
+
+            expect(max).to.equal(5);
+        });
+        it('should return the maximum value using a comparator function if one is provided', () => {
+            const oldest: {} = Stream.of([{ name: 'Al', age: 30 }, { name: 'Bob', age: 40}, { name: 'Cathy', age: 25 }])
+                                     .max((p1, p2) => (p1.age > p2.age) ? 1 : -1);
+
+            expect(oldest).to.deep.equal({ name: 'Bob', age: 40 });
+        });
+    });
+
+    describe('.min()', () => {
+        it('should return the minimum value using the "<" operator if no comparator is provided', () => {
+            const max: number = Stream.of([2,4,5,1,3])
+                                      .min();
+
+            expect(max).to.equal(1);
+        });
+        it('should return the minimum value using a comparator function if one is provided', () => {
+            const oldest: {} = Stream.of([{ name: 'Al', age: 30 }, { name: 'Bob', age: 40}, { name: 'Cathy', age: 25 }])
+                                     .min((p1, p2) => (p1.age > p2.age) ? 1 : -1);
+
+            expect(oldest).to.deep.equal({ name: 'Cathy', age: 25 });
+        });
+    });
+
+});
